b547741a30d24946080f77b15177bf737e6205ab
Lucianosouza888 <lucianoeliass42@gmail.com>
2025-06-24 11:57:05 -0300

    Add files via upload

---
+++ README (1).md
@@ -0,0 +1,3 @@
+# TSL Control App
+
+App PWA para controle de revisão de máquinas baseado em horímetro.
---
+++ app (1).js
@@ -0,0 +1,2 @@
+
+document.getElementById('root').innerHTML = '<h2 style="text-align:center;margin-top:2rem;">TSL Control PWA</h2><p style="text-align:center;">App instalado com sucesso!</p>';
